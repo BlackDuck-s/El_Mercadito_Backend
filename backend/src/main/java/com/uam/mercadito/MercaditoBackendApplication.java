9885c637f4dd8b840fb119623d958e9c805de9d5
Eduardo M. <yayomar211@gmail.com>
2025-11-12 13:56:30 -0600

    feat(backend): generate Spring Boot 3.3.5 scaffold with mvnw and core deps

---
+++ backend/src/main/java/com/uam/mercadito/MercaditoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uam.mercadito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaditoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaditoBackendApplication.class, args);
+	}
+
+}
